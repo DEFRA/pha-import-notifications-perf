a085672120751488e977ed528e0627c503ca2c56
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-11-21 09:36:14 +0000

    PHAIN-69: Add test scenario for `/hello/world` endpoint (#1)
    * PHAIN-69: Update test scenarios
    
    * PHAIN-69: Update entrypoint script
    
    * PHAIN-69: Update README

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 echo "run_id: $RUN_ID in $ENVIRONMENT"
 
@@ -19,25 +19,25 @@ REPORTFILE=${NOW}-perftest-${TEST_SCENARIO}-report.csv
 LOGFILE=${JM_LOGS}/perftest-${TEST_SCENARIO}.log
 
 # Run the test suite
-jmeter -n -t ${SCENARIOFILE} -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE} -f -Jenv="${ENVIRONMENT}"
+jmeter -n -t ${SCENARIOFILE} -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE} -f -Jprotocol="${TEST_PROTOCOL}" -Jhostname="${TEST_HOSTNAME}" -Jport="${TEST_PORT}"
 test_exit_code=$?
 
 # Publish the results into S3 so they can be displayed in the CDP Portal
 if [ -n "$RESULTS_OUTPUT_S3_PATH" ]; then
   # Copy the CSV report file and the generated report files to the S3 bucket
-   if [ -f "$JM_REPORTS/index.html" ]; then
-      aws --endpoint-url=$S3_ENDPOINT s3 cp "$REPORTFILE" "$RESULTS_OUTPUT_S3_PATH/$REPORTFILE"
-      aws --endpoint-url=$S3_ENDPOINT s3 cp "$JM_REPORTS" "$RESULTS_OUTPUT_S3_PATH" --recursive
-      if [ $? -eq 0 ]; then
-        echo "CSV report file and test results published to $RESULTS_OUTPUT_S3_PATH"
-      fi
-   else
-      echo "$JM_REPORTS/index.html is not found"
-      exit 1
-   fi
+  if [ -f "$JM_REPORTS/index.html" ]; then
+    aws --endpoint-url=$S3_ENDPOINT s3 cp "$REPORTFILE" "$RESULTS_OUTPUT_S3_PATH/$REPORTFILE"
+    aws --endpoint-url=$S3_ENDPOINT s3 cp "$JM_REPORTS" "$RESULTS_OUTPUT_S3_PATH" --recursive
+    if [ $? -eq 0 ]; then
+      echo "CSV report file and test results published to $RESULTS_OUTPUT_S3_PATH"
+    fi
+  else
+    echo "$JM_REPORTS/index.html is not found"
+    exit 1
+  fi
 else
-   echo "RESULTS_OUTPUT_S3_PATH is not set"
-   exit 1
+  echo "RESULTS_OUTPUT_S3_PATH is not set"
+  exit 1
 fi
 
 exit $test_exit_code
